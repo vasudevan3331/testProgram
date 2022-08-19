7e2c1fe14b886c4d76b62432e1c8e000b04d184d
vasudevan3331 <vasudevan3331@gmail.com>
2022-08-18 22:46:34 -0700

    intilization git

---
+++ basicmarks.java
@@ -0,0 +1,36 @@
+class a
+{
+public static void main(String[] total)
+{
+System.out.println("80/100");
+}
+}
+
+class b
+{
+public static void main(String[] total)
+{
+System.out.println("70/100");
+}
+}
+class c
+{
+public static void main(String[] total)
+{
+System.out.println("90/100");
+}
+}
+class d
+{
+public static void main(String[] total)
+{
+System.out.println("50/100");
+}
+}
+class f
+{
+public static void main(String[] total)
+{
+System.out.println("100/100");
+}
+}
---
+++ marks.java
@@ -0,0 +1,23 @@
+class arun
+{ 
+public static void main (String[] marks)
+{
+System.out.println("arun get first mark");
+}
+}
+
+class mani
+{
+public static void main (String[] marks)
+{
+System.out.println("mani get second mark");
+}
+}
+
+class siva
+{
+pubilc static void main (String[] marks)
+{
+System.out.println("siva get just pass mark");
+}
+}
