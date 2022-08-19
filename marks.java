a38be20b54b91195bbb4d4fb20d6cc62d786de37
vasudevan3331 <vasudevan3331@gmail.com>
2022-08-18 23:37:07 -0700

    hi

--- marks.java
+++ marks.java
@@ -6,18 +6,12 @@ System.out.println("arun get first mark");
 }
 }
 
-class mani
-{
-public static void main (String[] marks)
-{
-System.out.println("mani get second mark");
-}
-}
+
 
 class siva
 {
 pubilc static void main (String[] marks)
 {
-System.out.println("siva get just pass mark");
+System.out.println("get just pass mark");
 }
 }
