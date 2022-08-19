ac48f2f065c54a7408d46cc046de9676fc204d2b
vasudevan3331 <vasudevan3331@gmail.com>
2022-08-18 23:14:47 -0700

    game git

--- basicmarks.java
+++ basicmarks.java
@@ -10,14 +10,14 @@ class b
 {
 public static void main(String[] total)
 {
-System.out.println("70/100");
+System.out.println("100/100");
 }
 }
 class c
 {
 public static void main(String[] total)
 {
-System.out.println("90/100");
+System.out.println("70/100");
 }
 }
 class d
